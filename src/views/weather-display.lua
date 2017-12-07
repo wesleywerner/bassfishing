ee1b7372e1347d5db822c11add11da2ba99a681e
Wesley <wesley.werner@gmail.com>
2017-12-07 20:54:20 +0200

    Refactor drawing weather icons.

--- src/views/weather-display.lua
+++ src/views/weather-display.lua
@@ -18,19 +18,7 @@
 
 ]]--
 
-local module = {
-
-    icons = nil,
-
-    -- positions of drawn items
-    -- All drawings a relative to the info bar position
-    daypos = { 5, 2 },
-    iconpos = { 60, 30 },
-    temppos = { 30, 86, 140, "right" },
-    cloudpos = { 30, 116, 140, "right" },
-    windpos = { 30, 146, 140, "right" },
-
-}
+local module = { }
 
 function module:draw()
 
@@ -39,13 +27,17 @@ function module:draw()
     love.graphics.setFont(game.fonts.medium)
     love.graphics.setColor(game.color.base1)
 
-    -- print temp, cloud cover and wind
-    love.graphics.printf(string.format("%d°C", weather.airTemperature),
-        unpack(self.temppos))
-    love.graphics.printf(string.format("%d%%", weather.cloudcover),
-        unpack(self.cloudpos))
-    love.graphics.printf(string.format("%dkph %s",
-        weather.windSpeed, weather.windDirection), unpack(self.windpos))
+    -- print temp
+    love.graphics.draw(self.icons.image, self.icons.smallthermometer, 0, 86)
+    love.graphics.print(string.format("%d°C", weather.airTemperature), 40, 86)
+
+    -- print cloud cover
+    love.graphics.draw(self.icons.image, self.icons.smallcloud, 0, 116)
+    love.graphics.print(string.format("%d%%", weather.cloudcover), 40, 116)
+
+    -- print wind
+    love.graphics.draw(self.icons.image, self.icons.smallwind, 0, 146)
+    love.graphics.print(string.format("%dkph %s", weather.windSpeed, weather.windDirection), 40, 146)
 
     -- the weather icon priority is:
     -- approaching cold fronts
@@ -75,7 +67,7 @@ function module:draw()
         icon = self.icons.overcast
     end
 
-    love.graphics.draw(self.icons.image, icon, unpack(self.iconpos))
+    love.graphics.draw(self.icons.image, icon, 60, 30)
 
     love.graphics.pop()
 
@@ -86,23 +78,37 @@ if not module.icons then
     module.icons = { }
     module.icons.image = love.graphics.newImage("res/weather-icons.png")
     module.icons.w, module.icons.h = module.icons.image:getDimensions()
+    local w, h = module.icons.w, module.icons.h
 
     -- hot days
-    module.icons.hot = love.graphics.newQuad(0, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.hot = love.graphics.newQuad(1, 1, 60, 60, w, h)
+
     -- windy days
-    module.icons.windy = love.graphics.newQuad(60, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.windy = love.graphics.newQuad(62, 1, 60, 60, w, h)
+
     -- lighty overcast
-    module.icons.overcast = love.graphics.newQuad(120, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.overcast = love.graphics.newQuad(123, 1, 60, 60, w, h)
+
     -- clear
-    module.icons.clear = love.graphics.newQuad(180, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.clear = love.graphics.newQuad(184, 1, 60, 60, w, h)
+
     -- rain
-    module.icons.rainy = love.graphics.newQuad(240, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.rainy = love.graphics.newQuad(245, 1, 60, 60, w, h)
+
     -- cloudy and windy
-    module.icons.cloudygusts = love.graphics.newQuad(300, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.cloudygusts = love.graphics.newQuad(306, 1, 60, 60, w, h)
+
     -- heavy clouds
-    module.icons.cloudy = love.graphics.newQuad(360, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.cloudy = love.graphics.newQuad(367, 1, 60, 60, w, h)
+
     -- cold front
-    module.icons.coldfront = love.graphics.newQuad(420, 0, 60, 60, module.icons.w, module.icons.h)
+    module.icons.coldfront = love.graphics.newQuad(428, 1, 60, 60, w, h)
+
+    -- small icons
+    module.icons.smallthermometer = love.graphics.newQuad(489, 1, 30, 30, w, h)
+    module.icons.smallcloud = love.graphics.newQuad(520, 1, 30, 30, w, h)
+    module.icons.smallwind = love.graphics.newQuad(551, 1, 30, 30, w, h)
+    module.icons.smallclock = love.graphics.newQuad(582, 1, 30, 30, w, h)
 
 end
 
