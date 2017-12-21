e71b9cf770cd45b7efe2d2f8c0eb8a2ad0d161af
Wesley <wesley.werner@gmail.com>
2017-12-21 07:07:24 +0200

    Tidy up template and add responsive images.

--- site-src/make.sh
+++ site-src/make.sh
@@ -3,8 +3,5 @@ echo Ensure you run zim and add the basslover notebook to the list of known note
 
 zim --export basslover --output ../ --overwrite --template ./.template/basslover.html
 
-# add responsive images
-#find ../ -iname "*.html" -type f -exec sed -i 's/<img /<img class="img-responsive img-thumbnail" /g' {} +
-
 cd ..
 python -m SimpleHTTPServer
